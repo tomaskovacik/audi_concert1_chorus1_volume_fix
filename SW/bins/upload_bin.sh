f91d83b9f68c3dff5dbb6d4b5f2be7688c2fb4a8
Tomas kovacik <nail@nodomain.sk>
2021-10-25 12:47:39 +0200

    new stm32duino core version

--- SW/bins/upload_bin.sh
+++ SW/bins/upload_bin.sh
@@ -1,1 +1,1 @@
-${HOME}/.arduino15/packages/stm32duino/tools/stm32tools/2021.5.21/linux/maple_upload ttyACM0 2 1EAF:0003 ${1}
+${HOME}/.arduino15/packages/stm32duino/tools/stm32tools/2021.5.31/linux/maple_upload ttyACM0 2 1EAF:0003 ${1}

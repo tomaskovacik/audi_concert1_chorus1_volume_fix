350648cd76c63e70e8974b37b815d98698b54877
Tomas kovacik <kovacik@dgtfactory.com>
2026-05-16 11:31:29 +0200

    no bootloader = not ttyACM port

--- SW/bins/upload_bin.sh
+++ SW/bins/upload_bin.sh
@@ -1,1 +1,2 @@
-${HOME}/.arduino15/packages/stm32duino/tools/stm32tools/2021.5.31/linux/maple_upload ttyACM0 2 1EAF:0003 ${1}
+#${HOME}/.arduino15/packages/stm32duino/tools/stm32tools/2021.5.31/linux/maple_upload ttyACM0 2 1EAF:0003 ${1}
+./st-flash write ${1} 0x8000000
